14a63d30cb138d7f1dcddf6d5b3afbb5c0683b60
vishnumallipudi <vishnumallipudi.584@gmail.com>
2017-10-17 09:23:14 -0700

    Filtering operators

---
+++ AggregateOperatorsDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AggregateOperatorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var countries = new List<string>() { "india", "US", "China" };
+            Console.WriteLine(countries.Min(c=>c.Length));
+            Console.WriteLine($"Maximum value is: {nums.Max()}");
+            Console.WriteLine($"Minimum value is: {nums.Min()}");
+            Console.WriteLine($"Sum of all values is: {nums.Sum()}");
+            //average() returns Double
+            Console.WriteLine($"average of all values is: {nums.Average()}");
+            Console.WriteLine($"Number of values is: {nums.Count()}");
+
+
+            var appended = countries.Aggregate((a,b)=>a+", "+b);
+            Console.WriteLine("Sum of all values using aggregate operator is "+nums.Aggregate((a, b) => a * b)  );
+            Console.WriteLine("Sum of all values multiplied by 10  using aggregate operator is " + nums.Aggregate(10,(a, b) => a * b));
+
+            Console.WriteLine(appended);
+
+
+        }
+    }
+}
---
+++ FilteringOperatorsDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringOperatorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //where and OfType
+
+            var mylist = new ArrayList(5);
+            mylist.Add("vishnu");
+            mylist.Add("siababa");
+            mylist.Add(5);
+            mylist.Add("naveen");
+            mylist.Add("shreedhar");
+
+            var stringlist = mylist.OfType<string>();
+            Console.WriteLine("Only strings are");
+            foreach (var item in stringlist)
+            {
+                Console.Write(item+" ");
+            }
+            Console.WriteLine();
+            var index = stringlist.Max(a => a.Length);
+            var longlist = stringlist.Where(s => s.Length == index);
+            foreach (var item in longlist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Without  foreach");
+            var x = longlist.GetEnumerator();
+            while (x.MoveNext())
+            {
+                Console.WriteLine(x.Current);
+                
+            }
+            
+        }
+    }
+}
