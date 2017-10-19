466ffac560b1bfcd2803083a237b14e1f486af7e
vishnumallipudi <vishnumallipudi.584@gmail.com>
2017-10-19 13:38:50 -0700

    Join- Group join, inner join, leftouterjoin, cross join

---
+++ JoinsDemo/Department.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace JoinsDemo
+{
+    class Department
+    {
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+        public Department()
+        {
+
+        }
+        public Department(int id,string name)
+        {
+            DeptId = id;
+            DeptName = name;
+        }
+
+        public static IEnumerable<Department> GetAllDepartments()
+        {
+            return new List<Department> {
+                new Department(1,"IT"),
+                new Department(2, "Finance"),
+                new Department(3, "Hardware")
+            };
+        }
+    }
+}
---
+++ JoinsDemo/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace JoinsDemo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public int DepartmentId { get; set; }
+        public string Gender { get; set; }
+        public Employee()
+        {
+
+        }
+        public Employee(int id, string fname, int deptId, string gend)
+        {
+            this.Id = id;
+            this.Firstname = fname;
+            this.DepartmentId = deptId;
+            Gender = gend;
+        }
+
+        public static List<Employee> AllEmployees()
+        {
+            return new List<Employee> {
+                new Employee(1,"Vishnu",1,"Male"),
+                new Employee(2,"Saibaba",2,"Female"),
+                new Employee(3,"Naveen",3,"Female"),
+                new Employee(4,"Venkat",1,"Female"),
+                new Employee(5,"Raguveer",2,"Male"),
+                new Employee(6,"Shreedhar",3,"Male"),
+                new Employee(7,"Maneesh",2,"Female"),
+                new Employee(8,"Mohan",3,"Male"),
+                new Employee() {Id=9,Firstname="SpecialEmployee" }
+            };
+
+        }
+    }
+}
